3cc8d4818542641280a9eb60bbaba4e93f710b1f
zijiren233 <pyh1670605849@gmail.com>
2023-03-14 00:20:56 +0800

    Fix: TopP correct

--- translate.go
+++ translate.go
@@ -23,7 +23,7 @@ type TranslationConfig struct {
 	Ctx                 context.Context
 	MaxTokens           int
 	Temperature         float32 // 0-2, 越高越随机
-	TopP                float32 // 0.1表示仅考虑包含最高前10%概率质量的令牌,推荐1.0
+	TopP                float32 // 0-1,0.1表示仅考虑包含最高前10%概率质量的令牌,推荐1.0
 	PresencePenalty     float32 // 介于-2.0和2.0之间的数字。正值会根据新标记到目前为止是否出现在文本中来惩罚它们，从而增加模型谈论新主题的可能性。
 	FrequencyPenalty    float32 // 介于-2.0和2.0之间的数字。正值会根据新符号在文本中的现有频率来惩罚它们，从而降低模型逐字重复同一行的可能性。
 	From, SelectedWords string
@@ -43,6 +43,9 @@ func (cfg *TranslationConfig) correct() {
 	}
 	if cfg.MaxTokens < 0 || cfg.MaxTokens > 4096 {
 		cfg.MaxTokens = DefaultMaxTokens
+	}
+	if cfg.TopP < 0 || cfg.TopP > 1 {
+		cfg.TopP = DefaultTopP
 	}
 	if cfg.Temperature < 0 || cfg.Temperature > 2 {
 		cfg.Temperature = DefaultTemperature
